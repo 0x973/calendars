db671943d20975c45e332002fd5e3d0eb3c30134
0x973 <19320096+0x973@users.noreply.github.com>
2025-03-04 16:40:49 +0000

    update

--- update_oil_price_calendar.py
+++ update_oil_price_calendar.py
@@ -1,39 +1,23 @@
 import datetime
-import calendar
 from icalendar import Calendar, Event
-import requests
+from chinese_calendar import is_holiday, is_workday
 
-def is_workday(date, holidays):
-    return date.weekday() < 5 and date not in holidays  # Monday to Friday are workdays, excluding holidays
+def add_workdays(start_date, workdays):
+    # 如果将到下一年，则直接返回
+    if start_date.month == 12 and start_date.day + workdays > 31:
+        return datetime.datetime(start_date.year + 1, 1, 1)
 
-def add_workdays(start_date, workdays, holidays):
     current_date = start_date
     days_added = 0
     while days_added < workdays:
         current_date += datetime.timedelta(days=1)
-        if is_workday(current_date, holidays):
+        if is_workday(current_date):
             days_added += 1
     return current_date
 
-def get_holidays_from_ics(url):
-    holidays = []
-    response = requests.get(url)
-    if response.status_code == 200:
-        cal = Calendar.from_ical(response.content)
-        for component in cal.walk():
-            if component.name == "VEVENT":
-                summary = str(component.get('summary'))
-                if "（休）" in summary:
-                    holidays.append(component.get('dtstart').dt)
-    return holidays
-
 # 定义年份和首次调整日期
 year = datetime.datetime.now().year
-first_adjustment_date = datetime.datetime(2025, 1, 2)  # 设定首次调整日期
-
-# 获取法定节假日
-holidays_url = 'https://calendars.icloud.com/holidays/cn_zh.ics'
-holidays = get_holidays_from_ics(holidays_url)
+first_adjustment_date = datetime.datetime(2024, 1, 3)  # 设定首次调整日期
 
 # 初始化iCalendar内容
 cal = Calendar()
@@ -43,8 +27,14 @@ cal.add('version', '2.0')
 # 生成每10个工作日的事件
 current_date = first_adjustment_date
 adjustment_count = 0
-while current_date.year == year:
-    if is_workday(current_date, holidays):
+current_year = current_date.year
+
+while current_date.year <= year:
+    if current_date.year != current_year:
+        current_year = current_date.year
+        adjustment_count = 0  # 重置调整计数器
+
+    if is_workday(current_date):
         adjustment_count += 1
         event = Event()
         event.add('summary', '油价调整')
@@ -54,12 +44,12 @@ while current_date.year == year:
         cal.add_component(event)
 
         # 移动到下一个10个工作日
-        current_date = add_workdays(current_date, 10, holidays)
+        current_date = add_workdays(current_date, 10)
     else:
         current_date += datetime.timedelta(days=1)
 
 # 将iCalendar内容写入文件
-with open(f'oil_price_adjustment_{year}.ics', 'wb') as f:
+with open(f'oil_price_adjustment.ics', 'wb') as f:
     f.write(cal.to_ical())
 
-print(f"iCalendar file 'oil_price_adjustment_{year}.ics' created successfully!")
+print(f"iCalendar file 'oil_price_adjustment.ics' created successfully!")
