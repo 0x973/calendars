13bd0254d2c7b9b29e4ba8b261b3c719499cbcab
0x973 <19320096+0x973@users.noreply.github.com>
2025-03-04 18:39:01 +0000

    update

--- update_oil_price_calendar.py
+++ update_oil_price_calendar.py
@@ -1,6 +1,29 @@
-import datetime
+import os, time, datetime
 from icalendar import Calendar, Event
 from chinese_calendar import is_holiday, is_workday
+import requests
+
+os.environ['TZ'] = 'Asia/Shanghai'
+time.tzset()  # 使时区设置生效
+
+def get_tj_92_price():
+    url = 'https://v2.xxapi.cn/api/oilPrice'
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    }
+
+    try:
+        response = requests.get(url, headers=headers, timeout=6.0)
+        if response.status_code == 200:
+            data = response.json()
+            for item in data.get('data', []):
+                if item.get("regionName") == '天津市':
+                    return item.get('n92', 0)
+        return 0
+    except (requests.RequestException, ValueError, KeyError):
+        return 0
 
 def add_workdays(start_date, workdays):
     # 如果将到下一年，则直接返回
@@ -40,7 +63,14 @@ while current_date.year <= year:
         event.add('summary', '油价调整')
         event.add('dtstart', current_date.replace(hour=23, minute=59, second=59))
         event.add('dtend', (current_date + datetime.timedelta(days=1)).replace(hour=0, minute=0, second=0))
-        event.add('description', f'这是今年第{adjustment_count}次油价调整。')
+        
+        description = f'这是今年第{adjustment_count}次油价调整'
+        if current_date.date() == datetime.datetime.now().date():
+            price = get_tj_92_price()
+            if price != 0:
+                description += f'，当前92号汽油{price}元/升'
+        event.add('description', description)
+
         cal.add_component(event)
 
         # 移动到下一个10个工作日
